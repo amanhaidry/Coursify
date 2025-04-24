e3350801bfcd14ad2858e93098294e64e3704dbc
amanhaidry <amanhaidry9480@gmail.com>
2025-04-24 23:29:12 +0530

    changing cors to check server working or not

--- Server/app.js
+++ Server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://coursify-1-thl9.onrender.com",
+    origin: "http://localhost:5173/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
