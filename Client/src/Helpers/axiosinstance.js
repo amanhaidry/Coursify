dfdeec5a058894ce5f8ad273cfa35b3889cb5b22
amanhaidry <amanhaidry9480@gmail.com>
2025-04-25 05:13:39 +0530

    undo axio base url

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://coursify-server-f836.onrender.com/api/v1" // Production server URL
-    : "http://localhost:5000/api/v1"; // Local server URL
+const BASE_URL = "https://coursify-server-f836.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.withCredentials=true;
 
 export default axiosInstance;
