06166bd04af226e4ff790ade0f2edf6caa2ee13c
amanhaidry <amanhaidry9480@gmail.com>
2025-04-25 05:07:21 +0530

    added .env file,updated base url in axio and cors in app.js

--- Client/src/Helpers/axiosinstance.js
+++ Client/src/Helpers/axiosinstance.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "https://coursify-server-f836.onrender.com/api/v1";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://coursify-server-f836.onrender.com/api/v1" // Production server URL
+    : "http://localhost:5000/api/v1"; // Local server URL
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
-axiosInstance.defaults.withCredentials=true;
+axiosInstance.defaults.baseURL = BASE_URL;
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
--- Server/app.js
+++ Server/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://your-client-url.onrender.com"], // Add your Render client URL here
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
